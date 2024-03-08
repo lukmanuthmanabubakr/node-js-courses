2d2a605b3c67f41a186584f4774c4b61f9abfbad
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-03-08 22:51:43 +0100

    Update subdir.js

--- auth-tut-10/route/subdir.js
+++ auth-tut-10/route/subdir.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path')
 
 router.get('^/$|index(.html)?', (req, res) => {
-    // res.sendFile('./views/index.html', {root: __dirname});
+    
     res.sendFile(path.join(__dirname, "..", 'views', "subdir", 'index.html'));
 })
 router.get('/text(.html)?', (req, res) => {
