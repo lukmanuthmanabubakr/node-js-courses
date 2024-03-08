a2807662748029daf6ff300ef47ba4f916ef39e4
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-03-08 22:52:06 +0100

    Update subdir.js

--- auth-tut-10/route/subdir.js
+++ auth-tut-10/route/subdir.js
@@ -7,7 +7,7 @@ router.get('^/$|index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, "..", 'views', "subdir", 'index.html'));
 })
 router.get('/text(.html)?', (req, res) => {
-    // res.sendFile('./views/index.html', {root: __dirname});
+   
     res.sendFile(path.join(__dirname, "..", 'views', "subdir", 'text.html'));
 })
 
