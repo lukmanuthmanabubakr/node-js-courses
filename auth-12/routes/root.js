a9af820a5c9c7243124ab054a6768140bd8c7fa4
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-03-26 19:49:32 +0100

    Update root.js

--- auth-12/routes/root.js
+++ auth-12/routes/root.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 router.get('^/$|index(.html)?', (req, res) => {
-    //res.sendFile('./views/in
+    //res.sendFile('
     res.sendFile(path.join(__dirname, '..', "views", 'index.html'))
 })
 
