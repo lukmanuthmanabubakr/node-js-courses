7807fe0c902b2a7c64d9e1e316d555f87502b45a
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-03-30 21:40:20 +0100

    Update server.js

--- express-js-tut-6/server.js
+++ express-js-tut-6/server.js
@@ -1,7 +1,5 @@
 // const express = require('express');
 // const app = express();
-// const path = require("path");
-
 // const PORT = process.env.PORT || 3500;
 
 // app.get("^/$|index(.html)?",(req, res) => {
