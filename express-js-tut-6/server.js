67724d14ba741085c31de0177498fb96e0650dd3
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-04-03 09:45:18 +0100

    Update server.js

--- express-js-tut-6/server.js
+++ express-js-tut-6/server.js
@@ -36,7 +36,7 @@ const PORT = process.env.PORT || 3500;
 //   res.redirect(301, path.join(__dirname, "views", "new-page.html"));
 // });
 
-// //catch all rou error 404 page
+// //catch all rou err
 // app.get('/*', (req, res) => {
 //   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 //   
