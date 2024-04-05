2ec0ac61ef7ffd2f71b696379b4276778f0e26ab
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-04-05 07:09:10 +0100

    Update server.js

--- router-tut-8/server.js
+++ router-tut-8/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3500
 
 app.use(logger)
 
-// syntax for creating custom middleware
+// syntax for  creating a standard custom middleware
 
 // app.use((req, res, next) => {
 //       console.log(`${req.method}\n${req.path}`)
