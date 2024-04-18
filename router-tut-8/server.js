8cba4fe90d25c0207c85ce132e73d05c15a782d8
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-04-18 17:53:11 +0100

    Update server.js

--- router-tut-8/server.js
+++ router-tut-8/server.js
@@ -12,8 +12,7 @@ app.use(logger)
 
 
 // app.use((req, res, next) => {
-//       console.log(`${req.method}\n${req.path}`)
-//  
+//       console.log(`${req.method}\n${req.pat
 
 // Content-type application/x-www-form-urlencoded
 
