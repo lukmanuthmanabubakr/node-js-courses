40618b1718d56569b52da18edc80c0fdb088a0e8
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-07-11 22:11:56 +0100

    Update subdir.js

--- mvc-design-pattern-tut-9/route/subdir.js
+++ mvc-design-pattern-tut-9/route/subdir.js
@@ -6,7 +6,7 @@ router.get('^/$|index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, "..", 'views', "subdir", 'index.html'));
 })
 router.get('/text(.html)?', (req, res) => {
-    // res.sendFile('./views/index.html'
+    // res.sendFile('./views
     res.sendFile(path.join(__dirname, "..", 'views', "subdir", 'text.html'));
 })
 
